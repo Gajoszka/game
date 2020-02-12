29c80fc0fe84cd00d0229c129c45d2c71bd97c9b
Agata Gaj (gaja2) <gaja2@coventry.ac.uk>
2020-02-12 23:22:07 +0000

    db

--- menu/KeyFunctions.cpp
+++ menu/KeyFunctions.cpp
@@ -13,18 +13,18 @@ void KeyFunctions::connection() {
 	MYSQL_RES* res;
 	conn = mysql_init(0);
 
-	conn = mysql_real_connect(conn, "localhost", "agata", "DanonkI09", "testdb", 3306, NULL, 0);
+	conn = mysql_real_connect(conn, "localhost", "agata", "DanonkI09", "roguelike", 3306, NULL, 0);
 
 	if (conn) {
 		puts("Successful connection to databse");
 
-		string query = "SELECT * FROM test";
+		string query = "SELECT * FROM keyFun";
 		const char* q = query.c_str();
 		qstate = mysql_query(conn, q);
 		if (!qstate) {
 			res = mysql_store_result(conn);
 			while (row = mysql_fetch_row(res)) {
-				printf("ID: %s, Value %s\n, row[0], row[1], row[2]");
+				printf("Key: %s- %s\t\n", row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10] );
 			}
 		} else {
 			cout << "Query failed: " << mysql_error(conn) << endl;
