e14bcc0e25a135b085851311feaa7321cce471d6
Agata Gaj (gaja2) <gaja2@coventry.ac.uk>
2020-02-12 12:48:08 +0000

    random

--- menu/Dungeon.cpp
+++ menu/Dungeon.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 Dungeon::Dungeon() {
-	//char room[15][15];                ctrl+c+k a potem ctrl+k+u
-	//for (int i = 0; i <= 15; i++) {
-	//	for (int j = 0; j <= 15; j++) {
-	//		cout << room[i][j];
-	//	}
-	//}
+	const char room[2] = { '#', '.' };
+	char level[15][15];               // ctrl+c+k a potem ctrl+k+u
+	for (int i = 0; i <= 15; i++) {
+		for (int j = 0; j <= 15; j++) {
+			cout << level[room[rand() % 2]][room[rand() % 2]];
+		}
+	}
+
+	
+	
 }
 
 void Dungeon::print() {
